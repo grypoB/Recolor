3c5cc35e8dcf5d5538349f4dca865c8945858c50
Alexandre Devienne <grypoB@gmail.com>
2014-10-31 15:47:30 +0100

    Simplify code : no malloc, no function
    Put VLA instead of malloc, removed all function
    Stop checking value of scanf :(

--- source/recolor.c
+++ source/recolor.c
@@ -12,8 +12,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h> // for sqrt() function
-
 
 #define MIN_SEUIL 0
 #define MAX_SEUIL 1
@@ -38,23 +36,6 @@ static void erreur_seuil(float seuil);
 static void erreur_seuil_non_croissant(float s1, float s2);
 static void erreur_seuil_non_distinct(float s1, float s2);
 
-// from a pixel's RGB values, return a single normalized value
-static float normalize(int rgb_values[], int max);
-
-// input function, act as scanf, but checks if the return value is correct 
-// params : pointer to var
-static void scan_int(int *nb_adress);
-static void scan_float(float *nb_adress);
-static void scan_string(char string[]);
-
-// memory allocation function
-// params : number of cell
-// output : pointer
-static float*  init_float_tab(int size);
-static float** init_2D_float_tab(int x, int y);
-
-// Free a 2D tab (allocated by init_2D_float_tab)
-static void free_2D_float_tab(float **pointer, int x);
 
 //-------------------------------------------------------------------
 int main(void)
@@ -65,24 +46,28 @@ int main(void)
 
     int nbR = 0; // nombre de couleurs de recoloriage
     int nbF = 0; // nombre de filtrages
-    float *seuils    = NULL; // seuils utilisés
-    float **couleurs = NULL; // tableau des couleurs de recoloriage
 
     char format[FORMAT_SIZE] = {0};
     int nbC = 0; // nombre de colonne
     int nbL = 0; // nombre de ligne
     int intensite_max = 0;
     int rgb_values[COLOR_COMPONENTS] = {0};
-    float **image = NULL; // 2D array of the image's pixel
 
-    scan_int(&verbose); // verbose value is assumed correct
+    /* Variable lenght array
+    float seuils[];
+    float couleurs[][];
+    float image[][];
+    */
+
+
+    scanf("%d", &verbose); // verbose value is assumed correct
 
     if (verbose) printf("Entrez le nombre de couleurs de recoloriage :\n");
-    scan_int(&nbR);
+    scanf("%d", &nbR);
     if (nbR<MIN_RECOLOR_NB || nbR>MAX_RECOLOR_NB)
         erreur_nbR(nbR);
 
-    couleurs = init_2D_float_tab(nbR+1, COLOR_COMPONENTS);
+    float couleurs[nbR+1][COLOR_COMPONENTS];
     for (i=0 ; i<COLOR_COMPONENTS ; i++) // init border color
         couleurs[0][i] = BORDER_COLOR;
 
@@ -92,18 +77,18 @@ int main(void)
     {
         for (j=0 ; j<COLOR_COMPONENTS ; j++) // scan RBG components
         {
-            scan_float(&couleurs[i][j]);
+            scanf("%f", &couleurs[i][j]);
             if (couleurs[i][j]<MIN_COLOR || couleurs[i][j]>MAX_COLOR)
                 erreur_couleur(couleurs[i][j]);
         }
     }
 
-    seuils = init_float_tab(nbR-1);
+    float seuils[nbR-1];
 
     if (verbose) printf("Entrez les %d seuils de recoloriage :\n", nbR-1);
     for (i=0 ; i<nbR-1 ; i++) // scan seuils
     {
-        scan_float(&seuils[i]);
+        scanf("%f", &seuils[i]);
         if (seuils[i]<=MIN_SEUIL || seuils[i]>=MAX_SEUIL)
             erreur_seuil(seuils[i]);
         else if (seuils[i]<MIN_SEUIL+TOLERANCE_SEUIL)
@@ -120,18 +105,18 @@ int main(void)
     }
 
     if (verbose) printf("Entrez le nombre de filtrage :\n");
-    scan_int(&nbF); // nbF is assumed correct
+    scanf("%d", &nbF); // nbF is assumed correct
 
     // all values are assumed correct from this point
     if (verbose) printf("Entrez le code du format de l'image :\n");
-    scan_string(format);
+    scanf("%s", format);
     if (verbose) printf("Entrez les dimensions de l'image :\n");
-    scan_int(&nbC);
-    scan_int(&nbL);
+    scanf("%d", &nbC);
+    scanf("%d", &nbL);
     if (verbose) printf("Entrez l'intensité max pour la couleur : \n");
-    scan_int(&intensite_max);
+    scanf("%d", &intensite_max);
     
-    image = init_2D_float_tab(nbC, nbL);
+    //float image[nbC][nbL];
 
     if (verbose) printf("Entrez les valeurs des couleurs des pixels :\n");
     for (i=0 ; i<nbC ; i++)
@@ -140,120 +125,15 @@ int main(void)
         {
             for (k=0 ; k<COLOR_COMPONENTS ; k++)
             {
-                scan_int(&rgb_values[k]);
+                scanf("%d", &rgb_values[k]);
             }
-            image[i][j] = normalize(rgb_values, intensite_max);
+            //image[i][j] = normalize(rgb_values, intensite_max);
         }
-    }
+   }
 
     correct();
 
-    free(seuils);
-    free_2D_float_tab(image, nbC);
-    free_2D_float_tab(couleurs, nbR+1);
 	return EXIT_SUCCESS;
-}
-
-
-// From a pixel's RGB values, return a single normalized value
-static float normalize(int rgb_values[], int max)
-{
-    int i;
-    float result = 0;
- 
-    for (i=0 ; i<COLOR_COMPONENTS ; i++)
-        result += pow(rgb_values[i], 2);
-
-    return sqrt(result) / (sqrt(COLOR_COMPONENTS) * max);
-}
-
-
-// Scan an integer from the default input and exit on failure
-static void scan_int(int *nb_adress)
-{
-    if (scanf("%d", nb_adress) != 1)
-    {
-        printf("ERROR : Input failed (expected [%%d])\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-// Scan a float from the default input and exit on failure
-static void scan_float(float *nb_adress)
-{
-    if (scanf("%f", nb_adress) != 1)
-    {
-        printf("ERROR : Input failed (expected [%%f])\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-// Scan a string from the default input and exit on failure
-/* RISKY
- * If the string is longer than the memory allowed
- * it will override values in the memory
- */
-static void scan_string(char string[])
-{
-    if (scanf("%s", string) != 1)
-    {
-        printf("ERROR : Input failed (expected a string)\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-// Allocate memory for a 1D tab
-static float* init_float_tab(int size)
-{
-    float *pointer = NULL;
-
-    pointer = (float *) malloc(size*sizeof(float));
-
-    if (pointer == NULL)
-    {
-        printf("ERROR : Memory allocation failed (%lu bytes)\n", size*sizeof(float));
-        exit(EXIT_FAILURE);
-    }
-
-    return pointer;
-}
-
-
-// Allocate memory for a 2D tab
-static float** init_2D_float_tab(int x, int y)
-{
-    int i;
-    float **pointer = NULL;
-
-    pointer = (float**) malloc(x*sizeof(float*));
-    if (pointer == NULL)
-    {
-        printf("ERROR : Memory allocation failed (%lu bytes)\n", x*sizeof(float*));
-        exit(EXIT_FAILURE);
-    }
-
-    for (i=0 ; i<x ; i++)
-    {
-        pointer[i] = init_float_tab(y);
-    }
-
-    return pointer;
-}
-
-// Free the memory from a 2D tab (initialized by init_2D_tab())
-static void free_2D_float_tab(float **pointer, int x)
-{
-    int i;
-
-    for (i=0 ; i<x ; i++)
-    {
-        free(pointer[i]);
-    }
-
-    free(pointer);
 }
 
 
@@ -297,7 +177,6 @@ static void erreur_seuil(float seuil)
 static void erreur_seuil_non_croissant(float s1, float s2)
 {
 	printf("Valeurs de seuils non croissantes: %5.3f > %5.3f\n", s1, s2);
-		   
 	exit(EXIT_FAILURE);
 }
 
@@ -308,6 +187,5 @@ static void erreur_seuil_non_distinct(float s1, float s2)
 {
 	printf("Valeur de seuil trop proche du précédent [%5.3f]:"
 	       " %5.6f et %5.6f\n",TOLERANCE_SEUIL, s1, s2);
-		   
 	exit(EXIT_FAILURE);
 }
